374b463c39bb69689e1bedd84ac2a1d37e331b5c
Michael Sumner <mdsumner@gmail.com>
2022-11-30 09:42:41 +1100

    s

--- R/discretize.R
+++ R/discretize.R
@@ -28,7 +28,7 @@ discretize <- function(x, nrow = 50, ncol = 30, ..., draw = TRUE) {
   }
 #  polyi <- sample(1:nrow(p$object), 1)
 #  idx <- as.matrix(p$object$topology_[[polyi]][c(".vx0", ".vx1")])
-idx <- as.matrix(do.call(rbind, p$object$topology_)[c(".vx0", ".vx1")])
+idx <- do.call(rbind, lapply(p$object$topology_, \(xx) matrix(unlist(xx[c(".vx0", ".vx1")], use.names = FALSE), ncol = 2)))
   xx <- cbind(p$vertex$x_[idx[,1]],
               p$vertex$x_[idx[,2]])
   yy <- cbind(p$vertex$y_[idx[,1]],
@@ -49,13 +49,13 @@ idx <- as.matrix(do.call(rbind, p$object$topology_)[c(".vx0", ".vx1")])
   edges <- cbind(.x0 = xx[,1], .y0 = yy[,1],
                  .x1 = xx[,2], .y1 = yy[,2])
   ## sort edges top to bottom, left to right
-  edges <- as.matrix(dplyr::arrange(tibble::as_tibble(edges), dplyr::desc(.y0), .x0))
+ edges <- as.matrix(dplyr::arrange(tibble::as_tibble(edges), .x0, dplyr::desc(.y0)))
 
   ## slope of edge
-  edges <- cbind(edges,
-                 slope = (edges[,".y1"] - edges[,".y0"]) /
-                   (edges[,".x1"] - edges[,".x0"]))
-  edges[!is.finite(edges[, "slope"]), "slope"] <- 0
+  #edges <- cbind(edges,
+  #               slope = (edges[,".y1"] - edges[,".y0"]) /
+  #                 (edges[,".x1"] - edges[,".x0"]))
+  #edges[!is.finite(edges[, "slope"]), "slope"] <- 0
   resy <- raster::res(r)[2L]
   resx <- raster::res(r)[1L]
 
@@ -65,20 +65,27 @@ idx <- as.matrix(do.call(rbind, p$object$topology_)[c(".vx0", ".vx1")])
   if (draw) plot(p$vertex$x_, p$vertex$y_, asp = 1, pch = ".")
   nfill <- 0
   for (irow in seq_along(row_ys)) {
-    active <- edges[,".y0"] >= row_ys[irow] & edges[,".y1"] < row_ys[irow]
+    active <- edges[,".y0"] > row_ys[irow] & edges[,".y1"] <= row_ys[irow]
     if (any(active)) {
       (idx <- which(active))
-      (ycount <- ceiling((edges[idx, ".y0"] - row_ys[irow]) / resy))
-      (s <- edges[idx, "slope"])
-      (x0 <- edges[idx, ".x0"])
+      active_edges <- edges[active, ]
+      #active_edges <- as.matrix(dplyr::arrange(tibble::as_tibble(active_edges), .x0, dplyr::desc(.y0)))
+
+
+
+      (ycount <- ceiling((active_edges[, ".y0"] - row_ys[irow]) / resy))
+      s = (active_edges[,".y1"] - active_edges[,".y0"]) /
+                   (active_edges[,".x1"] - active_edges[,".x0"])
+      (x0 <- active_edges[, ".x0"])
       s <- 1/s
       s[!is.finite(s)] <- 0
       (xx <- x0 + -s * resy * ycount)
-
       fill <- matrix(findInterval(sort(xx), col_xs),
                      ncol = 2, byrow = TRUE)
       nfill <- nfill + dim(fill)[1L]
+
       if (draw) draw_fill(col_xs, fill, row_ys[irow])
+
     }
   }
 #  abline(h = row_ys, v = col_xs)
